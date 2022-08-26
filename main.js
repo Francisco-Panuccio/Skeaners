aaa933815e813a24d8c107ce573bd3d5d56db97c
Francisco Panuccio <franciscopanuccio@hotmail.com>
2022-08-26 00:50:47 -0300

    Corrigiendo JS

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let edad = parseInt(prompt("Ingrese su edad:"));
 
-while (edad <=0 || edad >= 100){
+while (edad <=10 || edad >= 100){
     edad = parseInt(prompt("Ingrese su edad nuevamente:"));   
 }
 
@@ -25,24 +25,44 @@ switch (estilo) {
 }
     
 let talle = parseInt(prompt("Ingrese su talle:"));
-if (talle < 45 && edad > 20 && estilo == "lifestyle"){
-    alert("Te recomendamos las Amidas VS Pace");
+
+while (talle > 50){
+    talle = parseInt(prompt("Ingrese su talle nuevamente:")); 
 }
-else if(talle >20 && edad <45 && estilo == "running"){
-    alert("Te recomendamos las Pluma Pro Classic");
-}
-else if (talle <45 && edad >=40 && edad <= 100 && estilo == "running"){
+
+if (talle > 45 && edad < 30 && estilo == "running"){
     alert("Te recomendamos las Phila Trend");
 }
-else if (talle > 45 && edad >= 40 && edad <= 100 && estilo == "running"){
-    alert("Te recomendamos las Cappa Logo Asti");
-}
-else if (talle >45 && edad >20 && estilo == "training"){
-    alert("Te recomendamos las Tupper Drive");
-}
-else if (talle <45 && edad >20 && estilo == "training"){
+else if(talle > 45 && edad < 30 && estilo == "training"){
     alert("Te recomendamos las Tupper Ultralight II Mesh");
 }
-else{
-    alert("Vuelve a indicar el talle por favor")
-};
+else if (talle > 45 && edad < 30 && estilo == "lifestyle"){
+    alert("Te recomendamos las Like SB Force 58 Premium");
+}
+else if (talle < 45 && edad < 30 && estilo == "running"){
+    alert("Te recomendamos las Pluma Pro Classic");
+}
+else if (talle < 45 && edad < 30 && estilo == "training"){
+    alert("Te recomendamos las Tupper Ultralight II Mesh");
+}
+else if (talle < 45 && edad < 30 && estilo == "lifestyle"){
+    alert("Te recomendamos las Amidas VS Pace");
+}
+else if (talle > 45 && edad > 30 && estilo == "running"){
+    alert("Te recomendamos las Cappa Logo Asti");
+}
+else if (talle > 45 && edad > 30 && estilo == "training"){
+    alert("Te recomendamos las Tupper Drive");
+}
+else if (talle > 45 && edad > 30 && estilo == "lifestyle"){
+    alert("Te recomendamos las Like SB Zoom Blazer Low Pro");
+}
+else if (talle < 45 && edad > 30 && estilo == "running"){
+    alert("Te recomendamos las Tupper Drive");
+}
+else if (talle < 45 && edad > 30 && estilo == "training"){
+    alert("Te recomendamos las Phila Trend");
+}
+else if (talle < 45 && edad > 30 && estilo == "lifestyle"){
+    alert("Te recomendamos las Amidas Grand Court");
+}
